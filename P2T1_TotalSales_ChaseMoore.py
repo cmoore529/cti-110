f7a8ce743a50b33f384e3d7e8efdc49eeb188a06
cmoore529 <44269446+cmoore529@users.noreply.github.com>
2018-11-08 12:19:13 -0500

    Add files via upload

---
+++ P2T1_TotalSales_ChaseMoore.py
@@ -0,0 +1,23 @@
+#CTI 110
+#M2T1
+#Chase Moore
+#11/1/18
+
+
+
+studentName = "Chase"
+studentGPA = "3.2"
+studentAge = "25"
+
+
+print("Hello", studentName)
+print(studentGPA)
+print(studentAge)
+
+# Get the projected total sales.
+total_sales = float(input('Enter the projected sales: '))
+
+# Calculate the profit as 23 percent of total sales
+profit = total_sales * 0.23
+
+print('The profit is $', profit)
