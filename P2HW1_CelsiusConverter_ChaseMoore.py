f001bdf2641a0f8345432b42b6f9c0cc340c198c
cmoore529 <44269446+cmoore529@users.noreply.github.com>
2018-11-08 12:12:49 -0500

    Add files via upload

---
+++ P2HW1_CelsiusConverter_ChaseMoore.py
@@ -0,0 +1,16 @@
+# This program is a celsius to Farenheit converter
+# 11/8/18
+# CTI-110 P2HW1- Celsius Farenheit Converter
+# Chase Moore
+
+
+#Ask for temperature in celsius
+celsiusTemp = int(input("Enter the temperature: "))
+
+#convert the temperature to Farenheit
+FarenheitTemp = 9/5*celciusTemp + 32
+
+#Tell the user the temperature
+print("The temperature is", FarenheitTemp)
+
+
---
+++ P2HW2_MaleFemale_Percentage_ChaseMoore.py
@@ -0,0 +1,24 @@
+# This program calculates percentages for male and females in a class
+# CTI-110 P2HW2 - Male and Female Percentages
+# 11/8/10
+# Chase Moore
+
+#Ask for number of male students in class
+NumOfMales = int(input("Enter the amount of males "))
+
+#Ask for number of female students in class
+NumOfFemales = int(input("Enter the amount of females "))
+
+#Calculate the percentage of males
+PercentageOfMales = NumOfMales / .20
+
+#calculate the percentage of females.
+PercentageOfFemales = NumOfFemales /.20
+
+#print the percentage of males
+print("The percentage of males in the class is", PercentageOfMales)
+
+#print the percentage of females
+print("The percentage of females in the class is", PercentageOfFemales)
+
+
