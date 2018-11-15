0d7c9d34d617c0f09e92f80cde361a072d5e4c08
cmoore529 <44269446+cmoore529@users.noreply.github.com>
2018-11-15 11:44:39 -0500

    Add files via upload

---
+++ P3LAB_Moore _1_.py
@@ -0,0 +1,44 @@
+# I was supposed to put a comment here
+# My Last Name
+
+def main():
+    # This program takes a number grade and outputs a letter grade.
+
+    # system uses 10-point grading scale
+    A_score = 90
+    B_score = 80
+    C_score = 70
+    D_score = 60
+    F_score = 50
+    # TO DO: define the rest of the scores
+
+    
+    score = int(input('Enter grade: '))
+
+    if score >= A_score:
+        print('Your grade is a: A')
+    else:
+        if score >= B_score:
+            print('Your grade is a: B')
+        else:
+                if score >= C_score:
+                     print('Your grade is a: C')
+                else:
+                        if score >= D_score:
+                             print('Your grade is a: D')
+                        else:
+                            if score  >= F_score:
+                                 print('Your grade is a: F')
+                                 
+                                 # TO DO: finish this
+                                           
+                                           
+
+
+
+
+
+
+
+# program start
+main()
