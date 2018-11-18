f5d0d9d7e6699d9ae74a8b550917448c13d79e8b
cmoore529 <44269446+cmoore529@users.noreply.github.com>
2018-11-17 21:34:22 -0500

    Add files via upload

---
+++ P3HW1_RomanNumerals_ChaseMoore.py
@@ -0,0 +1,65 @@
+#CTI-110
+#P3HW1
+#Chase Moore
+#11/15/18
+
+def main():
+#This is a program to display Roman Numerals
+
+    # Numbers 1-10
+    N1 = '1'
+    N2 = '2'
+    N3 = '3'
+    N4 = '4'
+    N5 = '5'
+    N6 = '6'
+    N7 = '7'
+    N8 = '8'
+    N9 = '9'
+    N10 = '10'
+
+    Number = str(input('Enter your number: '))
+
+    if Number == N1:
+        print('Your number is: I')
+    else:
+        if Number == N2:
+            print('Your number is: II')
+        else:
+                if Number == N3:
+                    print('Your number is: III')
+                else:
+                        if Number == N4:
+                            print('Your number is: IV')
+                        else:
+                                if Number == N5:
+                                    print('Your number is: V')
+                                else:
+                                        if Number == N6:
+                                            print('Your number is :VI')
+                                        else:
+                                                if Number == N7:
+                                                    print('Your number is: VII')
+                                                else:
+                                                        if Number == N8:
+                                                            print('Your number is: VIII')
+                                                        else:
+                                                                if Number == N9:
+                                                                    print('Your number is: IX')
+                                                                else:
+                                                                        if Number == N10:
+                                                                            print('Your number is: X')
+                                                                        elif Number >= N10:
+                                                                                    print('Your number is too high')
+                                                                                
+                                                                    
+                        
+                                
+            
+           
+
+
+
+
+
+main()
